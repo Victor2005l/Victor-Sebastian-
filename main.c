b7af7c925beb49b9d07f86fd33559657d327f45a
Victor2005l <sebastiantavarez532@gmail.com>
2024-09-24 17:12:26 -0400

    programa 1.4

--- main.c
+++ main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
-/* Promedio de sueldos.
-El programa, al recibir  como datos seis sueldos de un empleado, calcula tanto el ingreso total como el promedio  mensual.
+/* Superficie del triangulo.
+El programa, al recibir como datos la base y la altura de un triangulo, calcula su superficie.
 
-CLA: variable de tipo entero.
-SU1, SU2, SU3, SU4, SU5, SU6, ING, PRO: variable de tipo real. */
+BAS, ALT y SUP: variables de tipo real. */
 
-void main (void)
+void main(void)
 {
-    int CLA;
-    float SU1, SU2, SU3, SU4, SU5, SU6, ING, PRO;
-    printf("Ingrese la clave del empleado y los 6 sueldos: \n");
-    scanf("%d %f %f %f %f %f %f", &CLA, &SU1, &SU2, &SU3, &SU4, &SU5, &SU6);
-    ING = (SU1 + SU2 + SU3 + SU4 +SU5 + SU6);
-    PRO = ING / 6;
-    printf("\n %d %5.2f %5.2f" , CLA, ING, PRO);}
+
+}
